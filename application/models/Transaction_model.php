be82e5e82465cd43885e13ab8d068605cedfdacf
jfabricante <fabricantejerome@gmail.com>
2018-08-19 22:02:17 +0800

    Store new resource

--- application/models/Transaction_model.php
+++ application/models/Transaction_model.php
@@ -7,4 +7,53 @@ class Transaction_Model extends CI_Model {
 	{
 		$this->load->database();
 	}
+
+	/*
+	 * @args array
+	 */
+	public function store($args)
+	{
+		$trans = array(
+			'trans_total' => $args['trans_total'] ? $args['trans_total'] : 0,
+			'cash'        => $args['cash']	 ? $args['cash'] : 0,
+			'change'      => $args['change'] ? $args['change'] : 0,
+			'cashier_id'  => $this->session->userdata('id') ? $this->session->userdata('id') : 0
+		);
+
+		$this->db->trans_begin();
+		$this->db->insert('transaction_tbl', $trans);
+
+		if ($this->db->trans_status() == FALSE) {
+			$this->db->trans_rollback();
+
+			return 0;
+		}
+		else {
+			$id = $this->db->insert_id();
+
+			$this->db->trans_commit();
+
+			return $id;
+		}
+	}
+
+	/*
+	 * @args array
+	 */
+	public function storeItems($args)
+	{
+		$this->db->trans_begin();
+		$this->db->insert_batch('transaction_item_tbl', $args);
+
+		if ($this->db->trans_status() == FALSE) {
+			$this->db->trans_rollback();
+
+			return false;
+		}
+		else {
+			$this->db->trans_commit();
+
+			return true;
+		}
+	}
 }
