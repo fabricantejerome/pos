247afbb0baadfe61d81c10e291078322a313fddf
jfabricante <fabricantejerome@gmail.com>
2018-09-11 15:08:41 +0800

    Fetch items

--- application/models/Items_model.php
+++ application/models/Items_model.php
@@ -10,13 +10,16 @@ class Items_model extends CI_Model {
 
 	public function fetch()
 	{
-		$fields = ['it.id', 'it.product_line', 'it.style_number', 'it.size', 'it.color', 'it.quantity', 'it.price', 'it.barcode', 'ct.name AS category'];
-		
-		$query = $this->db->select($fields)
-				->from('items_tbl AS it')
-				->join('category_tbl AS ct', 'it.category_id = ct.id', 'INNER')
-				->order_by('it.product_line', 'ASC')
-				->get();
+		$query = $this->db->query('SELECT it.id, it.product_line, it.style_number, it.size, it.color, it.price, it.barcode,
+					CASE 
+						WHEN tit.quantity > 0 THEN it.quantity - SUM(tit.quantity)
+						ELSE it.quantity
+					END AS quantity, ct.name AS category
+					FROM items_tbl AS it 
+					LEFT JOIN transaction_item_tbl AS tit ON it.id = tit.item_id
+					INNER JOIN category_tbl AS ct ON it.category_id = ct.id
+					GROUP BY it.id
+					ORDER BY it.product_line');
 
 		return $query->result();
 	}
