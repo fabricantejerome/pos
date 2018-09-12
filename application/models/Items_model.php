be8f9ddcd6f97a29b4e175a6f877a4f6b0ca19c3
jfabricante <fabricantejerome@gmail.com>
2018-09-12 22:50:42 +0800

    Change mode

--- application/models/Items_model.php
+++ application/models/Items_model.php
@@ -10,16 +10,16 @@ class Items_model extends CI_Model {
 
 	public function fetch()
 	{
-		$query = $this->db->query('SELECT it.id, it.product_line, it.style_number, it.size, it.color, it.price, it.barcode,
+		$query = $this->db->query("SELECT it.id, it.product_line, it.style_number, it.size, it.color, it.price, it.barcode,
 					CASE 
-						WHEN tit.quantity > 0 THEN it.quantity - SUM(tit.quantity)
+						WHEN SUM(tit.quantity) > 0 THEN it.quantity - SUM(tit.quantity)
 						ELSE it.quantity
 					END AS quantity, ct.name AS category
 					FROM items_tbl AS it 
 					LEFT JOIN transaction_item_tbl AS tit ON it.id = tit.item_id
 					INNER JOIN category_tbl AS ct ON it.category_id = ct.id
-					GROUP BY it.id
-					ORDER BY it.product_line');
+					GROUP BY it.id, tit.item_id
+					ORDER BY it.product_line");
 
 		return $query->result();
 	}
